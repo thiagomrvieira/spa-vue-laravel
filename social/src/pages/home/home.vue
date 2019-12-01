a77249e1e5cd7a66d58d90956155dff74379e47a
Thiago Maciel <thiagomrvieira@gmail.com>
2019-12-01 19:37:53 +0000

    Diretorios do projeto

---
+++ social/src/pages/home/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h2>Social</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
